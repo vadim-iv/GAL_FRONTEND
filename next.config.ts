30bacf10b54a44fe0799f97fdfe105a6ac0c555d
VadimOMEGA <stejaruldacilor2023@gmail.com>
2025-09-10 18:03:15 +0300

    changed the default locale in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  i18n: {
+    defaultLocale: 'ro',
+    locales: ['ro', 'en', 'ru'],
+  },
   images: {
     remotePatterns: [
       {
