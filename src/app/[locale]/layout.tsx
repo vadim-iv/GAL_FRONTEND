24d9f0fc566caf86ff21fe316742801454d6e001
VadimOMEGA <vadimivanov.0320@gmail.com>
2025-09-23 13:40:34 +0300

    Made favicon adapt to theme

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,13 +15,17 @@ import './globals.css'
 export const metadata: Metadata = {
 	icons: {
 		icon: [
-			{ url: '/favicon-16.png', sizes: '16x16', type: 'image/png' },
-			{ url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
-			{ url: '/favicon-48.png', sizes: '48x48', type: 'image/png' },
-			{ url: '/favicon-96.png', sizes: '96x96', type: 'image/png' },
-			{ url: '/favicon-196.png', sizes: '196x196', type: 'image/png' }
-		],
-		shortcut: '/favicon.ico'
+			{
+				rel: 'icon',
+				media: '(prefers-color-scheme: light)',
+				url: '/favicons/favicon-dark.svg'
+			},
+			{
+				rel: 'icon',
+				media: '(prefers-color-scheme: dark)',
+				url: '/favicons/favicon-light.svg'
+			}
+		]
 	}
 }
 
